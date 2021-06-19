67653e1d889af2f0277ba4c6129e65d3889338d6
sivashankar1205 <86148623+sivashankar1205@users.noreply.github.com>
2021-06-19 18:35:57 +0800

    Add files via upload
    testing upload

---
+++ StartTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class StartTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		for (int j= 0; j<n; j++){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
